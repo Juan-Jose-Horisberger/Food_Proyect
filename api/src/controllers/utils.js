a66963a6290ffd69c7bc8d87985bb71959b957d3
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-09-16 11:55:59 -0300

    Hacemos cambios en la pre carga de info (recipes)

--- api/src/controllers/utils.js
+++ api/src/controllers/utils.js
@@ -5,32 +5,86 @@ const { YOUR_API_KEY } = process.env;
 
 const dbLoader = async () => {
     try {
-        const api = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
+        // const api = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
 
-        if (api) {
-            const apiInfo = api.data.results?.map(e => {
-                return {
-                    //id: e.id, //Quitar este id ya que lo generamos solo desde el db
-                    name: e.title,
-                    summary: e.summary.replace(/<[^>]*>?/gm, ""),
-                    healthScore: e.healthScore,
-                    analyzedInstructions: e.analyzedInstructions
-                        ? e.analyzedInstructions.map(a => a.steps.map((b, i) => `${i + 1}. ${b.step}.`)).flat()
-                        : "",
-                    image: e.image,
-                    dietsApi: e.diets
+        // if (api) {
+        //     const apiInfo = api.data.results?.map(e => {
+        //         return {
+        //             //id: e.id, //Quitar este id ya que lo generamos solo desde el db
+        //             name: e.title,
+        //             summary: e.summary.replace(/<[^>]*>?/gm, ""),
+        //             healthScore: e.healthScore,
+        //             analyzedInstructions: e.analyzedInstructions
+        //                 ? e.analyzedInstructions.map(a => a.steps.map((b, i) => `${i + 1}. ${b.step}.`)).flat()
+        //                 : "",
+        //             image: e.image,
+        //             dietsApi: e.diets
+        //         }
+        //     })
+
+        //     const allInfo = Recipe.findAll({ include: Diet })
+        //     if (!allInfo.length) {
+        //         for (let i = 0; i < apiInfo.length; i++) {
+        //             await Recipe.create(apiInfo[i])
+        //         }
+        //     }
+        // }
+
+        const recipes = await axios(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
+
+        // formatea todas los elem filtrados de la api
+        const recipesApi = recipes.data.results.map(result => {
+
+            // formateo en un obj lo necesario
+            const recipeFormat = {
+                id: result.id,
+                name: result.title,
+                summary: result.summary?.replace(/<\/?[^>]+(>|$)/g, ""),
+                healthScore: result.healthScore,
+                analyzedInstructions: result.analyzedInstructions[0] ? result.analyzedInstructions[0].steps.map(data => data.step) : ["..."], // lo agrega solo si existe
+                image: result.image,
+                dietsApi: result.diets
+            };
+            return recipeFormat;
+        });
+
+        let dietsFound = [];
+
+        for (let i = 0; i < recipesApi.length; i++) {
+
+            const api = recipesApi[i];
+
+            // me fijo que no exista
+            const exists = await Recipe.findOne({ where: { name: api.name } });
+
+            if (!exists) {
+                // creo en la db la nueva Receta
+                const newRecipe = await Recipe.create({
+                    id: api.id,  // si comento el id lo podria autogenerar y hacer que todas sean editables          
+                    name: api.name,
+                    summary: api.summary,
+                    healthScore: api.healthScore,
+                    analyzedInstructions: api.analyzedInstructions,
+                    image: api.image
+                });
+
+                for (let j = 0; j < api.diets.length; j++) {
+                    const diet = api.diets[j]; // nombre de dieta
+                    const findDiet = await Diet.findOne({ where: { name: diet } }); // busco en la db
+
+                    if (findDiet && findDiet.id) {
+                        console.log(findDiet.id); // id
+                        dietsFound.push(findDiet.id); // guardo los id 
+                    }
                 }
-            })
-
-            const allInfo = Recipe.findAll({ include: Diet })
-            if (!allInfo.length) {
-                for (let i = 0; i < apiInfo.length; i++) {
-                    await Recipe.create(apiInfo[i])
+                // relaciono la receta con la/las dieta/s 
+                if (dietsFound.length) {
+                    newRecipe.addDiets(dietsFound);
+                    dietsFound = [];
                 }
             }
-
-            // return apiInfo
         }
+        console.log("Recipes preloaded!");
     }
     catch (e) {
         console.log(e);
