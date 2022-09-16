bc1af5db9445d22a48bb64446cc9dab55e31d955
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-09-16 12:13:41 -0300

    Cambios en la ruta get

--- api/src/routes/Recipes.js
+++ api/src/routes/Recipes.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const router = express();
+const { Recipe, Diet } = require("../db.js");
 const { getAllInfo, recipeById, addInfo, orderByName } = require('../controllers/Recipes.controller.js');
 
 
 
 // GET
 
-router.get('/', getAllInfo);
+// router.get('/', getAllInfo);
+
+router.get("/", async (req, res) => {
+
+    const { name } = req.query;
+
+    try {
+        let recipesDB;
+
+        // si tengo query filtro sobre ella
+        if (name) {
+            recipesDB = await Recipe.findAll({ where: { name: { [Op.iLike]: `%${name}%` } }, include: [{ model: Diet }] });
+        } else {
+            recipesDB = await Recipe.findAll({ include: [{ model: Diet }] });
+        }
+
+        if (!recipesDB.length) {
+            return res.status(404).send({ error: "Recipe not found!" });
+        }
+
+        return res.status(200).send(recipesDB);
+
+    } catch (error) {
+        return res.status(400).send({ error: error.message });
+    }
+});
+
+
+
 router.get('/:id', recipeById);
-// router.get('/diet/:diet', getRecipesForDiet);
 router.get('/name/:order', orderByName)
 
 // POST
